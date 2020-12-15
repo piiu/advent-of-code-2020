727d4167f7874b419eae7b6c34d11627ed0d8924
Piiu Pilt <piiu.pilt@helmes.com>
2020-12-15 11:30:33 +0200

    Approved readability

--- src/Common/BaseDay.php
+++ src/Common/BaseDay.php
@@ -14,7 +14,7 @@ abstract class BaseDay
 
     public function __construct()
     {
-        ini_set('memory_limit', '2048M');
+        ini_set('memory_limit', '512M');
         $dayNumber = preg_replace("/[^0-9]/", "", get_class($this));
         $inputFile = __DIR__ . '\..\..\input\day' . $dayNumber;
         if (file_exists($inputFile)) {
--- src/Days/Day15.php
+++ src/Days/Day15.php
@@ -8,13 +8,13 @@ class Day15 extends BaseDay
 {
     public function execute()
     {
-        $input = array_merge([null], $this->getInputArray(',')); // Force start from 1
-        $spokenNumbers = @array_flip($input);
+        $input = array_merge([''], $this->getInputArray(',')); // Force start from 1
+        $lastSpoken = array_flip($input);
 
         for ($i = count($input); $i <= 30000000; $i++) {
-            $speak = empty($spokenBefore) ? 0 : $i - 1 - $spokenBefore;
-            $spokenBefore = $spokenNumbers[$speak] ?? null;
-            $spokenNumbers[$speak] = $i;
+            $speak = empty($lastSpokenTurn) ? 0 : $i - 1 - $lastSpokenTurn;
+            $lastSpokenTurn = $lastSpoken[$speak] ?? null;
+            $lastSpoken[$speak] = $i;
 
             if ($i === 2020) {
                 $this->part1 = $speak;
