ec91134c1046bad1bd3dd5f5c0c84493da93f84a
Piiu Pilt <piiu.pilt@helmes.com>
2020-12-17 17:38:49 +0200

    Day 17 - This is how I roll

--- src/Days/Day17.php
+++ src/Days/Day17.php
@@ -16,38 +16,60 @@ class Day17 extends BaseDay
             return str_split($row);
         }, $this->getInputArray(PHP_EOL));
 
-        $state1 = [[$input]];
+        $state1 = [$input];
         $state2 = [[$input]];
 
         for ($i = 0; $i < 6; $i++) {
-            $this->expand($state1);
             $this->part1 = $this->cycle($state1);
-            $this->expand($state2, true);
             $this->part2 = $this->cycle($state2);
         }
     }
 
-    private function cycle(array &$state, int $activeCount = 0) : int
+    private function cycle(array &$state) : int
     {
         $fixedState = $state;
-        foreach ($fixedState as $w => $dimension) {
-            foreach ($dimension as $z => $layer) {
-                foreach ($layer as $y => $row) {
-                    foreach ($row as $x => $value) {
-                        $coordinates = [$w, $z, $y, $x];
-                        $neighbours = $this->getNeighboursCount($fixedState, $coordinates, $value === self::ACTIVE);
-                        $newState = $this->shouldBeActive($value, $neighbours) ? self::ACTIVE : self::INACTIVE;
-                        $state = Arr::set($state, $coordinates, $newState);
+        $depths = $this->getMinMaxByDepth($fixedState);
+        return $this->goDeeper($state, $fixedState, $depths);
+    }
 
-                        if ($newState === self::ACTIVE) {
-                            $activeCount++;
-                        }
-                    }
-                }
+    private function goDeeper(array &$state, array $fixedState, array $depths, array $coordinates = [], $activeCount = 0) : int
+    {
+        if (!$depths) {
+            if ($this->applyState($state, $fixedState, $coordinates) === self::ACTIVE) {
+                $activeCount++;
             }
+            return $activeCount;
         }
-
+        list($min, $max) = array_shift($depths);
+        for ($i = $min; $i <= $max; $i++) {
+            $activeCount = $this->goDeeper($state, $fixedState, $depths, array_merge($coordinates, [$i]), $activeCount);
+        }
         return $activeCount;
+    }
+
+    private function applyState(array &$state, array $fixedState, $coordinates) : string
+    {
+        $value = Arr::get($state, $coordinates) ?? self::INACTIVE;
+        $neighbours = $this->getNeighboursCount($fixedState, $coordinates, $value === self::ACTIVE);
+        $newState = $this->shouldBeActive($value, $neighbours) ? self::ACTIVE : self::INACTIVE;
+        $state = Arr::set($state, $coordinates, $newState);
+        return $newState;
+    }
+
+    private function shouldBeActive(string $value, int $neighbours) : bool
+    {
+        return (($value === self::ACTIVE && in_array($neighbours, [2, 3]))
+            || ($value === self::INACTIVE && $neighbours === 3));
+    }
+
+    private function getMinMaxByDepth(array $state, array $depths = []) : array
+    {
+        $keys = array_keys($state);
+        $depths[] = [min($keys) - 1, max($keys) + 1];
+        if (is_array($state[0])) {
+            return $this->getMinMaxByDepth($state[0], $depths);
+        }
+        return $depths;
     }
 
     private function getNeighboursCount(array $state, array $coordinates, bool $self = true, int $number = 0) : int
@@ -65,28 +87,5 @@ class Day17 extends BaseDay
             }
         }
         return $self ? $number - 1 : $number;
-    }
-
-    private function shouldBeActive(string $value, int $neighbours) : bool
-    {
-        return (($value === self::ACTIVE && in_array($neighbours, [2, 3])) || ($value === self::INACTIVE && $neighbours === 3));
-    }
-
-    private function expand(array &$state, bool $is4d = false)
-    {
-        $wKeys = array_keys($state);
-        $wRange = $is4d ? range(min($wKeys)-1, max($wKeys)+1) : [0];
-        foreach ($wRange as $w) {
-            $zKeys = array_keys($state[0]);
-            foreach (range(min($zKeys) - 1, max($zKeys) + 1) as $z) {
-                $yKeys = array_keys($state[0][0]);
-                foreach (range(min($yKeys) - 1, max($yKeys) + 1) as $y) {
-                    $xKeys = array_keys($state[0][0][0]);
-                    foreach (range(min($xKeys) - 1, max($xKeys) + 1) as $x) {
-                        $state[$w][$z][$y][$x] = $state[$w][$z][$y][$x] ?? self::INACTIVE;
-                    }
-                }
-            }
-        }
     }
 }
