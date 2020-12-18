5fca045b240653d6ec70528d089acb903dfe039f
Piiu Pilt <piiu.pilt@helmes.com>
2020-12-18 17:10:21 +0200

    Day 18 - shorten

--- src/Days/Day18.php
+++ src/Days/Day18.php
@@ -8,22 +8,21 @@ class Day18 extends BaseDay
 {
     public function execute()
     {
-        foreach ($this->getInputArray() as $expression) {
-            $this->part1 += $this->calculate($expression);
-            $this->part2 += $this->calculate($expression, true);
+        foreach ($this->getInputArray(PHP_EOL) as $exp) {
+            $this->part1 += $this->calculate($exp); // wrong: 30753705405211, right: 30753705453324
+            $this->part2 += $this->calculate($exp, '+'); // wrong: 244817530095800, right: 244817530095503
         }
     }
 
-    private function calculate(string $expression, bool $sumsFirst = false) : int
+    private function calculate(string $exp, string $first = '+*') : int
     {
-        $expression = '('.$expression.')';
-        $singlePattern = '/\d+ [' . ($sumsFirst ? '+' : '+*') . '] \d+/';
-        while (preg_match('/\(([^()]+)\)/', $expression, $inner)) {
-            while (preg_match($singlePattern, $inner[1], $single)) {
+        $exp = '('.$exp.')';
+        while (preg_match('/\(([^()]+)\)/', $exp, $inner)) {
+            while (preg_match("/\d+ [$first] \d+/", $inner[1], $single)) {
                 $inner[1] = str_replace($single[0], math_eval($single[0]), $inner[1]);
             }
-            $expression = str_replace($inner[0], math_eval($inner[1]), $expression);
+            $exp = str_replace($inner[0], math_eval($inner[1]), $exp);
         }
-        return $expression;
+        return $exp;
     }
 }
