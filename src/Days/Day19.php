7f3b11a1e1566a8efd277ce2f828ba84da2d9962
Piiu Pilt <piiu.pilt@helmes.com>
2020-12-19 15:23:28 +0200

    Day 19 - part 2 still a bit off

--- src/Days/Day19.php
+++ src/Days/Day19.php
@@ -6,65 +6,65 @@ use AdventOfCode\Common\BaseDay;
 
 class Day19 extends BaseDay
 {
-    private array $rules = [];
-
     public function execute()
     {
         list($ruleBlock, $messageBlock) = $this->getInputArray(PHP_EOL.PHP_EOL);
-        $this->formatRules($ruleBlock);
+        $rules = $this->formatRules($ruleBlock);
         $messages = explode(PHP_EOL, $messageBlock);
 
-        $this->part1 = $this->getNumberOfMatches($messages);
+        $this->part1 = $this->getNumberOfMatches($rules, $messages);
 
-        $this->rules[8] = '42 | 42 8';
-        $this->rules[11] = '42 31 | 42 11 31';
+        $rules[8] = '42 | 42 8';
+        $rules[11] = '42 31 | 42 11 31';
 
-        $this->part2 = $this->getNumberOfMatches($messages);
+        $this->part2 = $this->getNumberOfMatches($rules, $messages);
     }
 
-    private function getNumberOfMatches(array $messages) : int
+    private function getNumberOfMatches(array $rules, array $messages) : int
     {
         $matches = 0;
         foreach ($messages as $message) {
             $testIndex = 0;
-            if ($this->matches(str_split($message), $this->rules[0], $testIndex) && $testIndex >= strlen($message)) {
+            if ($this->matches(str_split($message), $rules, $rules[0], $testIndex) && $testIndex >= strlen($message)) {
                 $matches++;
             }
         }
         return $matches;
     }
 
-    private function matches(array $message, string $rule, int &$testIndex) : int
+    private function matches(array $message, array $rules, string $rule, int &$testIndex) : int
     {
-        if (strlen($rule) === 1 && !is_numeric($rule)) {
+        if (in_array($rule, ['a', 'b'])) {
             if (empty($message[$testIndex]) || $message[$testIndex] === $rule) {
                 $testIndex++;
                 return true;
             }
-            return false;
-        }
-        $currentIndex = $testIndex;
-        foreach (explode(' | ', $rule) as $option) {
-            $testIndex = $currentIndex;
-            $allMatch = true;
-            foreach (explode(' ', $option) as $ruleIndex) {
-                if (!$this->matches($message, $this->rules[$ruleIndex], $testIndex)) {
-                    $allMatch = false;
-                    break;
+        } else {
+            $currentIndex = $testIndex;
+            foreach (explode(' | ', $rule) as $option) {
+                $testIndex = $currentIndex; // reset
+                $allMatch = true;
+                foreach (explode(' ', $option) as $ruleIndex) {
+                    if (!$this->matches($message, $rules, $rules[$ruleIndex], $testIndex)) {
+                        $allMatch = false;
+                        break;
+                    }
                 }
-            }
-            if ($allMatch) {
-                return true;
+                if ($allMatch) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    private function formatRules(string $ruleBlock)
+    private function formatRules(string $ruleBlock) : array
     {
+        $rules = [];
         foreach (explode(PHP_EOL, $ruleBlock) as $ruleRow) {
             $parts = explode(': ', $ruleRow);
-            $this->rules[$parts[0]] = str_replace('"', '', $parts['1']);
+            $rules[$parts[0]] = str_replace('"', '', $parts['1']);
         }
+        return $rules;
     }
 }
